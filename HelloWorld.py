0350ca5fc28a1f89e666c7c01823cd13e0fc3f13
JerryShi <jerryshi2007@outlook.com>
2018-11-06 00:19:23 +0800

    First

---
+++ HelloWorld.py
@@ -0,0 +1,37 @@
+print('Hello,World')
+
+# print("Please input your name :")
+# name = input()
+# print('Hello, ', name)
+
+
+
+#print("1024*768 = ", 1024*768)
+
+
+# a = "ABC"
+# b = a
+# a = "XYZ"
+
+# print(b)
+
+
+n = 123
+print(n)
+f = 456.789
+print(f)
+s1 = "Hello,World"
+print(s1)
+s2 = "Hello,\'Adam\'"
+print(s2)
+s3 = r'Hello, "Bart"'
+print(s3)
+s4 = r'''Hello, 
+
+Lisa'''
+
+print(s4)
+
+print("Press any key to continue...")
+input()
+
