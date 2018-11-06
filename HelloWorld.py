c812d7873a48ed0c17b0b184a6a011f31972feae
JerryShi <jerryshi2007@outlook.com>
2018-11-06 23:54:46 +0800

    update Helloworld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,14 @@
 print('Hello,World')
 
+
+#----------------------------------------------
 # print("Please input your name :")
 # name = input()
 # print('Hello, ', name)
+#----------------------------------------------
 
 
+################################################
 
 #print("1024*768 = ", 1024*768)
 
@@ -15,22 +19,104 @@ print('Hello,World')
 
 # print(b)
 
+################################################
 
-n = 123
-print(n)
-f = 456.789
-print(f)
-s1 = "Hello,World"
-print(s1)
-s2 = "Hello,\'Adam\'"
-print(s2)
-s3 = r'Hello, "Bart"'
-print(s3)
-s4 = r'''Hello, 
 
-Lisa'''
 
-print(s4)
+################################################
+# n = 123
+# print(n)
+# f = 456.789
+# print(f)
+# s1 = "Hello,World"
+# print(s1)
+# s2 = "Hello,\'Adam\'"
+# print(s2)
+# s3 = r'Hello, "Bart"'
+# print(s3)
+# s4 = r'''Hello, 
+
+# Lisa'''
+
+# print(s4)
+##################################################
+
+
+##############################################
+# clasemates = ['Michel','Bob','Tracy']
+
+# print(len(clasemates))
+# print(clasemates[0])
+# print(clasemates[1])
+# print(clasemates[2])
+
+
+# print(clasemates[-1])
+
+
+# clasemates.append('Jerry')
+# print(clasemates[3])
+
+# clasemates.insert(2,'OBQ')
+# print(clasemates[2])
+
+
+# var = clasemates.pop()
+
+# print("var : "+var)
+
+# print("classmate index - 1 : " + clasemates[-1])
+
+
+###############################################
+
+
+# pvar = ["abc", 123, True]
+# print(pvar[-1])
+
+# svar = ['python', 'java', ['asp', 'php'], 'scheme']
+# print(svar[-1])
+
+
+# print(svar[2])
+
+
+###############################################
+
+# t = ('a','b',['A','B'])
+
+# t[2][0] = 'X'
+# t[2][1] = 'Y'
+
+
+# print(t[0])
+# print(t[2])
+
+
+###############################################
+
+
+age = 20
+if age >= 18:
+    print("age is ", age )
+    print("adult")
+else:
+    print("age is ", age)
+    print("teenager")
+
+
+age = 3
+if age >= 18:
+    print("age is ", age )
+    print("adult")
+elif age >= 6:
+    print("age is", age)
+    print("teenager")
+else:
+    print("age is ", age)
+    print("kid")
+
+
 
 print("Press any key to continue...")
 input()
